55e8a3f9df93cb404f59768614d68fddd48b1da9
natan colleoni <natancolleonifonseca@gmail.com>
2023-03-23 22:22:32 -0300

    manipulação pokeApi

--- src/atividades/Assincronia, Callbacks e Promises/20230323/exemplo.js
+++ src/atividades/Assincronia, Callbacks e Promises/20230323/exemplo.js
@@ -1,4 +1,4 @@
-import { writeFile }  from 'fs/promises'
+import { writeFile, readFile }  from 'fs/promises'
 
 export class RequestDataApi {
     
@@ -6,7 +6,34 @@ export class RequestDataApi {
         const pokeApiResponse = await fetch('https://pokeapi.co/api/v2/pokemon?limit=2')
 
         pokeApiResponse.json().then(result => {
-            writeFile('allPokeUrl.json', JSON.stringify(result.results))
+            writeFile('allPokeUrl.json', JSON.stringify(result.results, null, 2))
+        })
+    }
+
+    async fetchAllPokemonData() {
+        const allPokeUrl = JSON.parse(await readFile('allPokeUrl.json'))
+        
+        let pokeArr = []
+        //não performatica
+        allPokeUrl.map(async (pokemon) => {
+            let url = pokemon.url
+
+            const response = await fetch(url)
+            const result = await response.json()
+
+            const pokeData = {
+                name: result.forms[0].name,
+                dex: result.game_indices[9].game_index,
+                version: result.game_indices[9].version.name,
+                height: result.height,
+                weight: result.weight,
+            }
+
+            pokeArr.push(pokeData)
+            pokeArr.sort((a, b) => {
+                return a.dex - b.dex
+            })
+            writeFile('pokedex.json', JSON.stringify(pokeArr, null, 2))
         })
     }
 
